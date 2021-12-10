9f563198feb73239a8eba47e172152e00851dc06
AlexiusVictoria <lexyvt@gmail.com>
2021-12-09 22:38:09 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios'
+import Character from './components/Character'
+import CharacterInfo from './components/CharacterInfo'
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  const [characters, setCharacters] = useState([])
+  const [characterDetails, setCharacterDetails] = useState(null)
 
+  const openInfo = character => {
+    setCharacterDetails(character)
+  }
+  const closeInfo = () => {
+    setCharacterDetails(null)
+  }
+
+  useEffect(() => {
+    axios
+      .get('https://swapi.dev/api/people')
+      .then(response => {
+        setCharacters(response.data)
+        console.log(response.data)
+      })
+      .catch(error => {
+        console.log('Characters Error')
+      })
+  }, [])
+  
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {characters.map(character => {
+        return <Character key={character.id} info={character} action={openInfo} />
+      })}
+      {characterDetails && <CharacterInfo characterInfo={characterDetails} close={closeInfo} />}
     </div>
   );
 }
-
 export default App;
